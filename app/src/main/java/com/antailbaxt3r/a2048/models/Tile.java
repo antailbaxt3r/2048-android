02fb0f123f1953221ee51bed56d4bb1a329d2afb
Arjun Bajpai <arjunbajpaiwork@gmail.com>
2020-11-02 12:51:42 +0530

    Initial commit

---
+++ app/src/main/java/com/antailbaxt3r/a2048/models/Tile.java
@@ -0,0 +1,34 @@
+package com.antailbaxt3r.a2048.models;
+
+public class Tile extends Cell {
+    private final int value;
+    private Tile[] mergedFrom = null;
+
+    public Tile(int x, int y, int value) {
+        super(x, y);
+        this.value = value;
+    }
+
+    public Tile(Cell cell, int value) {
+        super(cell.getX(), cell.getY());
+        this.value = value;
+    }
+
+    public void updatePosition(Cell cell) {
+        this.setX(cell.getX());
+        this.setY(cell.getY());
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public Tile[] getMergedFrom() {
+        return mergedFrom;
+    }
+
+    public void setMergedFrom(Tile[] tile) {
+        mergedFrom = tile;
+    }
+}
+
